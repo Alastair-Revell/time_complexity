937ad2b42e1c40f90cfced7016c3a5c6fc0b958b
Student <ar15153@outlook.com>
2019-04-11 11:36:42 +0100

    bubble sort time graph

--- algos/bubble_sort.py
+++ algos/bubble_sort.py
@@ -1,23 +1,36 @@
 import random as rn
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
 def bubbleSort(arr):
+    start_time = time.process_time()
     n = len(arr)
     for i in range(n):
         for j in range(0, n-i-1):
             if arr[j] > arr[j+1] :
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-
-alist = list(range(500))
-rnd_order= rn.sample(alist, k=500)
-bubbleSort(rnd_order)
+    total_time = time.process_time() - start_time
+    time_list.append(total_time)
 
 timings = list(range(5000, 100001, 5000))
 lists = []
+time_list = []
 
 def random_lists(size):
-    alist = list(range(size))
-    rnd_list = rn.sample(alist, k=size)
+    rnd_list = rn.sample(range(1, size+1), size)
     lists.append(rnd_list)
     return lists
 
-print([random_lists(i) for i in timings])
+timing_lists = [random_lists(i) for i in timings]
+[bubbleSort(i) for i in timing_lists]
+import seaborn as sns
+
+sns.set_style("darkgrid")
+new_time_list = np.cumsum(time_list)
+plt.plot(timings, new_time_list, marker='o')
+plt.xlabel('List Size')
+plt.ylabel('CPU Time')
+plt.title('A plot examnining Bubble Sort Time Efficieny with varying List Sizes')
+plt.savefig('bubble.png')
